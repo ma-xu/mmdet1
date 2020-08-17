e46763bacd6de13ea201283625d8dccff104627d
Xu Ma <xuma@my.unt.edu>
2020-08-17 01:06:04 -0500

    update

--- mmdet/models/roi_heads/bbox_heads/bbox_head.py
+++ mmdet/models/roi_heads/bbox_heads/bbox_head.py
@@ -204,6 +204,7 @@ class BBoxHead(nn.Module):
         if isinstance(cls_score, list):
             cls_score = sum(cls_score) / float(len(cls_score))
         scores = F.softmax(cls_score, dim=1) if cls_score is not None else None
+        # print("Running bbox_head.py")
 
         if bbox_pred is not None:
             bboxes = self.bbox_coder.decode(
