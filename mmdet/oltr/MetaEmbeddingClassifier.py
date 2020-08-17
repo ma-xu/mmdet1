b41d0616bf3ceadae3d57e52e2dde2b8b1842890
Xu Ma <xuma@my.unt.edu>
2020-08-17 11:26:12 -0500

    Update MetaEmbeddingClassifier.py

--- mmdet/oltr/MetaEmbeddingClassifier.py
+++ mmdet/oltr/MetaEmbeddingClassifier.py
@@ -9,16 +9,18 @@ class MetaEmbedding_Classifier(nn.Module):
     
     def __init__(self, feat_dim=2048, num_classes=1000):
         super(MetaEmbedding_Classifier, self).__init__()
-        """
+
         self.num_classes = num_classes
+        """
         self.fc_hallucinator = nn.Linear(feat_dim, num_classes)
         self.fc_selector = nn.Linear(feat_dim, feat_dim)
-        # self.cosnorm_classifier = CosNorm_Classifier(feat_dim, num_classes)
         """
+        # self.cosnorm_classifier = CosNorm_Classifier(feat_dim, num_classes)
+
         self.normal_classifier = nn.Linear(feat_dim, num_classes)
         
     def forward(self, x,centroids):
-        """
+
         # Added by Nokia Intern Xu Ma
         # # consider the background class: padding zero
         # centroids = torch.cat([centroids,centroids[:,-1]-centroids[:,-1]],dim=0)
@@ -41,7 +43,7 @@ class MetaEmbedding_Classifier(nn.Module):
         scale = 10.0
         reachability = (scale / values_nn[:, 0]).unsqueeze(1).expand(-1, feat_size)
 
-
+        """
         # computing memory feature by querying and associating visual memory
         values_memory = self.fc_hallucinator(x.clone())
         values_memory = values_memory.softmax(dim=1)
@@ -52,12 +54,14 @@ class MetaEmbedding_Classifier(nn.Module):
         concept_selector = concept_selector.tanh()
 
         x = reachability * (direct_feature + concept_selector * memory_feature)
-
+        """
+        x = reachability * direct_feature
+        
         # storing infused feature
-        infused_feature = concept_selector * memory_feature
+        # infused_feature = concept_selector * memory_feature
         
         # logits = self.cosnorm_classifier(x)
-        """
+        
         logits = self.normal_classifier(x)
 
         # return logits, [direct_feature, infused_feature]
