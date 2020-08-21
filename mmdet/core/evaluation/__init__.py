d76cbd6d5f85e6e814d9e1e69527cb1f1541a47d
Xu Ma <xuma@my.unt.edu>
2020-08-21 12:22:13 -0500

    update

--- mmdet/core/evaluation/__init__.py
+++ mmdet/core/evaluation/__init__.py
@@ -8,7 +8,7 @@ from .recall import (eval_recalls, plot_iou_recall, plot_num_recall,
 
 __all__ = [
     'voc_classes', 'imagenet_det_classes', 'imagenet_vid_classes',
-    'coco_classes', 'cityscapes_classes', 'dataset_aliases', 'get_classes',
+    'coco_classes', 'cityscapes_classes', 'dataset_aliases', 'get_classes','coco_eval','results2json',
     'DistEvalHook', 'EvalHook', 'average_precision', 'eval_map',
     'print_map_summary', 'eval_recalls', 'print_recall_summary',
     'plot_num_recall', 'plot_iou_recall'
