0f8bf1d1139f16e798ec09f8199eaaf7e5141a8e
Xu Ma <xuma@my.unt.edu>
2020-08-21 12:26:13 -0500

    Update __init__.py

--- mmdet/core/evaluation/__init__.py
+++ mmdet/core/evaluation/__init__.py
@@ -5,6 +5,7 @@ from .eval_hooks import DistEvalHook, EvalHook
 from .mean_ap import average_precision, eval_map, print_map_summary
 from .recall import (eval_recalls, plot_iou_recall, plot_num_recall,
                      print_recall_summary)
+from .coco_utils import coco_eval,results2json
 
 __all__ = [
     'voc_classes', 'imagenet_det_classes', 'imagenet_vid_classes',
