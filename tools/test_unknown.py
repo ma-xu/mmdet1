59aaed3cdfb8844d1ee248e7e3dc42a686a1dd00
13952522076 <xuma@my.unt.edu>
2020-09-04 02:42:43 -0500

    update

--- tools/test_unknown.py
+++ tools/test_unknown.py
@@ -127,6 +127,7 @@ def single_gpu_test(model,
             print(new_bbox_result[0])
             print(new_bbox_result[37])
             # print("___________________")
+            print(f"DEBUG\n{bbox_result}")
             result = new_bbox_result,new_segm_result
 
         if show or out_dir:
